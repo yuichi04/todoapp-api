179a9e8fc75d37a6bbddc906fab78220de904177
yamashita yuichi <crgone910428@gmail.com>
2025-01-14 16:04:47 +0900

    修正

--- Dockerfile
+++ Dockerfile
@@ -6,14 +6,6 @@ WORKDIR /todoapp-api
 # Go Modulesを有効にする
 ENV GO111MODULE=on
 
-# データベース接続設定
-ENV POSTGRES_USER=postgres
-ENV POSTGRES_PW=postgres
-ENV POSTGRES_HOST=db
-ENV POSTGRES_PORT=5432
-ENV POSTGRES_DB=postgres
-ENV GO_ENV=dev
-
 # ローカルのモジュールキャッシュを最適化
 COPY go.mod .
 COPY go.sum .
--- db/db.go
+++ db/db.go
@@ -5,18 +5,11 @@ import (
 	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewDB() *gorm.DB {
-	if os.Getenv("GO_ENV") == "dev" {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatalln(err)
-		}
-	}
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
 		os.Getenv("POSTGRES_USER"),
 		os.Getenv("POSTGRES_PW"),
