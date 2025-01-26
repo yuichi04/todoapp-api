91980c412bfae8995e82038020757a3e903ed462
yamashita yuichi <crgone.910428.en@gmail.com>
2025-01-27 00:44:38 +0900

    タスクのCRUD機能を追加

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 require (
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
@@ -31,6 +32,7 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/echo-jwt/v4 v4.3.0
 	github.com/labstack/echo/v4 v4.13.3
 	golang.org/x/text v0.21.0 // indirect
 	gorm.io/driver/postgres v1.5.11
--- router/router.go
+++ router/router.go
@@ -6,12 +6,25 @@ import (
 	"os"
 	"todoapp-api/controller"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
+	e.Debug = true
+
+	// すべてのリクエストをログ出力
+	e.Use(middleware.Logger())
+
+	// リクエストの詳細をログ出力するミドルウェア
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			fmt.Printf("Request: %s %s\n", c.Request().Method, c.Request().URL.Path)
+			return next(c)
+		}
+	})
 
 	// CORSのミドルウェアを設定
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
@@ -37,26 +50,25 @@ func NewRouter(uc controller.IUserController) *echo.Echo {
 		// CookieMaxAge:   60,
 	}))
 
-	e.Debug = true
-
-	// すべてのリクエストをログ出力
-	e.Use(middleware.Logger())
-
-	// リクエストの詳細をログ出力するミドルウェア
-	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			fmt.Printf("Request: %s %s\n", c.Request().Method, c.Request().URL.Path)
-			return next(c)
-		}
-	})
-
 	e.GET("/test", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Test endpoint working!")
+		return c.String(http.StatusOK, "Test endpoint workinga!")
 	})
 
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.LogOut)
 	e.GET("/csrf", uc.CsrfToken)
+
+	t := e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+
+	t.GET("/tasks", tc.GetAllTasks)
+	t.GET("/:taskId", tc.GetTaskById)
+	t.POST("", tc.CreateTask)
+	t.PUT("/:taskId", tc.UpdateTask)
+	t.DELETE("/:taskId", tc.DeleteTask)
 	return e
 }
